77f2e87a355d7655bf790a0fa9a4aeea63c30359
Gene Backlin <gbacklin@marizack.com>
2017-09-12 21:00:39 -0500

    Updated php script

--- Docs/parameterPush.php
+++ Docs/parameterPush.php
@@ -1,20 +1,18 @@
 <?php
 
+$message = $_POST["message"];
+$deviceToken = $_POST["deviceToken"];
+$passphrase = $_POST["passphrase"];
+
 // Put your device token here (without spaces):
-//$deviceToken = 'cd591070c8d603cfc1d10046be48d82b6c581742cccf2a1e2808145a54ccf6c3';
 $iPhone5 = 'cd591070c8d603cfc1d10046be48d82b6c581742cccf2a1e2808145a54ccf6c3';
 $iPhone6 = '30465565af89b7d114259f4749509c9f77c5309d4b68a710fbc9674aa76df3b9';
 
-// Put your private key's passphrase here:
-//$passphrase = 'marissa123';
-
-// Put your alert message here:
-//$message = 'My first push notification!';
-
-$deviceToken = $_POST['deviceToken'];
-$passphrase = $_POST['passphrase'];
-$message = $_POST['message'];
-$deliverToToken = 'cd591070c8d603cfc1d10046be48d82b6c581742cccf2a1e2808145a54ccf6c3'
+if ( strcmp ( $deviceToken, $iPhone6) == 0  ){
+  $deviceToken=$iPhone5;
+} else{
+  $deviceToken=$iPhone6;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -32,6 +30,9 @@ if (!$fp)
 	exit("Failed to connect: $err $errstr" . PHP_EOL);
 
 echo 'Connected to APNS' . PHP_EOL;
+echo "message ", $message, "!";
+echo "deviceToken ", $deviceToken, "!";
+echo "passphrase ", $passphrase, "!";
 
 // Create the payload body
 $body['aps'] = array(
@@ -43,7 +44,7 @@ $body['aps'] = array(
 $payload = json_encode($body);
 
 // Build the binary notification
-$msg = chr(0) . pack('n', 32) . pack('H*', $deliverToToken) . pack('n', strlen($payload)) . $payload;
+$msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
 
 // Send it to the server
 $result = fwrite($fp, $msg, strlen($msg));
